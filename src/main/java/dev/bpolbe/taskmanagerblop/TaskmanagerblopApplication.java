0cb85ec5160c3142e7c6c8ea656552d813ba5aa5
ANGELINA <goblininstail@gmail.com>
2024-09-15 22:03:05 +0300

    first commit

---
+++ src/main/java/dev/bpolbe/taskmanagerblop/TaskmanagerblopApplication.java
@@ -0,0 +1,13 @@
+package dev.bpolbe.taskmanagerblop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerblopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerblopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bpolbe/taskmanagerblop/TaskmanagerblopApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bpolbe.taskmanagerblop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerblopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
